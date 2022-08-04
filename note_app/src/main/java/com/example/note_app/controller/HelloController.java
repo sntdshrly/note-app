11518409e74ec861e600392ec9533eb61192be5b
nisadvianii <nisadevianiruis17@gmail.com>
2022-08-04 09:46:49 +0700

    UI

--- note_app/src/main/java/com/example/note_app/controller/HelloController.java
+++ note_app/src/main/java/com/example/note_app/controller/HelloController.java
@@ -1,9 +1,19 @@
 package com.example.note_app.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
+    public Label labelStatus;
+    public Button btnCreate;
+    public Button btnSave;
+    public Button btnDelete;
+    public Button btnSettings;
+    public Button btnClose;
+    public ListView list1;
+    public TextField txtTitle;
+    public Label labelKeterangan;
+    public TextArea txtArea;
     @FXML
     private Label welcomeText;
 
