0954c8bd1f90f919efa2832bc07cb21116e05b1d
sherly santiadi <sntdshrly@gmail.com>
2022-08-14 21:02:20 +0700

    + css file for button

--- note_app/src/main/java/com/example/note_app/Main.java
+++ note_app/src/main/java/com/example/note_app/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+//        scene.getStylesheets().add(getClass().getResource("style/style.css").toExternalForm());
         stage.setTitle("Note App Application");
         stage.setScene(scene);
         stage.show();
